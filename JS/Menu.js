9ac5ac6da913d433182c9062921b0621cd5bab06
vonheinzenberg <peterheinzenberg@gmail.com>
2024-04-09 18:10:12 -0300

    update html e css
    atualizei o menu para ser mostrado em linha junto ao header

--- JS/Menu.js
+++ JS/Menu.js
@@ -1,22 +1,22 @@
-const dropdownBtn = document.getElementById("btn");
-const dropdownMenu = document.getElementById("dropdown");
-const toggleArrow = document.getElementById("arrow");
+// const dropdownBtn = document.getElementById("btn");
+// const dropdownMenu = document.getElementById("dropdown");
+// const toggleArrow = document.getElementById("arrow");
 
-// Toggle do menu
-const toggleDropdown = function () {
-  dropdownMenu.classList.toggle("show");
-  toggleArrow.classList.toggle("arrow");
-};
+// // Toggle do menu
+// const toggleDropdown = function () {
+//   dropdownMenu.classList.toggle("show");
+//   toggleArrow.classList.toggle("arrow");
+// };
 
-// Toggle de abrir e fechar quando é clicado
-dropdownBtn.addEventListener("click", function (e) {
-  e.stopPropagation();
-  toggleDropdown();
-});
+// // Toggle de abrir e fechar quando é clicado
+// dropdownBtn.addEventListener("click", function (e) {
+//   e.stopPropagation();
+//   toggleDropdown();
+// });
 
-// Fecha o dropdown qnd seleciona
-document.documentElement.addEventListener("click", function () {
-  if (dropdownMenu.classList.contains("show")) {
-    toggleDropdown();
-  }
-});
+// // Fecha o dropdown qnd seleciona
+// document.documentElement.addEventListener("click", function () {
+//   if (dropdownMenu.classList.contains("show")) {
+//     toggleDropdown();
+//   }
+// });
